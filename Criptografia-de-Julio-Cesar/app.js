3dab7b871a315cca3909706db4437f4be7a725eb
Alex Xavier <ajxv20@gmail.com>
2020-03-22 00:27:21 -0300

    Tentativa de atualizar o arquivo

--- Criptografia-de-Julio-Cesar/app.js
+++ Criptografia-de-Julio-Cesar/app.js
@@ -1,15 +1,34 @@
 const express = require ('express')
 const fetch = require ('node-fetch')
 const fs = require ('fs')
+const caesar = require ('caesar-encrypt')
+const CryptoJS = require ('crypto-js')
 
 const app = express()
 
 //app.use(express.static(__dirname + '/frontend'))
 
 app.listen(8001, () => {
-  console.log('Servidor rodando!')
+  console.log('Server on!')
 })
 
 fetch('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=a42a7bde25f7f1beb4303ba2ef068c9436f563d7')
   .then(res => res.text())
   .then(body => fs.writeFileSync('answer.json', body))
+
+caesar.decrypt('pszym', '5')
+
+const hash = CryptoJS.SHA1('you are bound to be unhappy if you optimize everything. donald e. knuth').toString()
+
+const fileBuffer = JSON.parse(fs.readFileSync('answer.json', 'utf-8'))
+
+fileBuffer.decifrado = 'you are bound to be unhappy if you optimize everything. donald e. knuth'
+fileBuffer.resumo_criptografico = hash
+
+console.log(fileBuffer)
+
+fs.writeFile('answer.json', fileBuffer, err => {
+  if (err) {
+    return console.error(err)
+  }
+})
